b8fd29f74d5bf240b1087e57bd73491aafc9c5bd
sovsh <soshatokh@yandex.ru>
2021-12-17 16:28:01 +0300

    add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#index'
+
+  post '/auth', to: 'auth#create'
+  get '/auth/new', to: 'auth#new'
+  delete '/auth/:id', to: 'auth#destroy'
+
+  post '/users', to: 'users#create'
+  get '/users/new', to: 'users#new'
+  get '/users/:id', to: 'users#show'
+  get '/users/:id/edit', to: 'users#edit'
+  put '/users/:id', to: 'users#update'
+  delete '/users/:id', to: 'users#destroy'
+
+  post '/books', to: 'books#create'
+  get '/books', to: '/books#index'
+  get 'books/:id', to: 'books#show'
+  get 'books/new', to: '/books#new'
+
+  post '/users/:user_id/books', to: 'books#create'
+  get 'users/:user_id/books/new', to: 'books#new'
+  get 'users/:user_id/books', to: 'books#index'
+  get 'users/:user_id/books/:id', to: 'books#show'
+  get 'users/:user_id/books/:id/edit', to: 'books#edit'
+  put 'users/:user_id/books/:id', to: 'books#update'
+  delete 'users/:user_id/books/:id', to: 'books#destroy'
+
 end
