4f9d24c3a459189708e8609565546851f088155a
sovsh <soshatokh@yandex.ru>
2021-12-20 21:39:28 +0300

    add authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  def authorise_user
+    redirect_to auth_path unless @current_user
+  end
+
+  def set_current_user
+    @current_user ||= User.find_by_id session[:user_id]
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.valid?
-      /session[:user_id] = @user.id/
-      redirect_to 'users/show'
+      session[:user_id] = @user.id
+      #redirect_to 'users/show' #redirect to the right path
     else
       redirect_to users_new_path, alert: @user.errors.full_messages.join
     end
